0c661e911a8b8bf206833da10e7fbc20602d364b
indigosaurus <indigosaurus@hotmail.co.uk>
2023-02-23 02:13:21 +0000

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as ServiceWorkerRegistration from './serviceWorkerRegistration';
+import * as ServiceWorkerRegistration from '../dist/registerSW';
 //import reportWebVitals from './reportWebVitals';
 
 /*
