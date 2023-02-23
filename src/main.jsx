5fba39ce6dbb8a85ea222f8f7b67edec1bd8f1e8
indigosaurus <indigosaurus@hotmail.co.uk>
2023-02-23 01:53:45 +0000

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as ServiceWorkerRegistration from './serviceWorkerRegistration';
+//import reportWebVitals from './reportWebVitals';
+
+/*
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+*/
+
+//localStorage.setItem('tasks',JSON.stringify(DATA));
+
+const DATA = JSON.parse(localStorage.getItem('tasks')) || [];
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//     <App tasks={DATA} />
+//   </React.StrictMode>,
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>
+);
+ServiceWorkerRegistration.register();
+//reportWebVitals();
